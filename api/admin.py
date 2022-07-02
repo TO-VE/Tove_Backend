92ef7c03347449628c3720a2e842935197247df5
chaeri93 <chaeri93@ewhain.net>
2022-07-03 01:54:27 +0900

    chore: admin update

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import *
 
+
+class VeganAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Vegan._meta.fields]
+
 admin.site.register(User)
-admin.site.register(Vegan)
+admin.site.register(Vegan, VeganAdmin)
 admin.site.register(CO2Cal)
 admin.site.register(Challenge)
 admin.site.register(GroupPurchase)
