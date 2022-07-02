5512b39f538befb13e4a0ec27b5d53d8ddcc7d0b
chaeri93 <chaeri93@ewhain.net>
2022-07-02 16:57:06 +0900

    allowed host update

--- Tove_Backend/settings.py
+++ Tove_Backend/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-xpu430xzdbt%b(bpfky-@=17es22fzimey=+e$j3l759*ru+i5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".ap-northeast-2.compute.amazonaws.com"
+]
 
 AUTH_USER_MODEL = 'api.User'
 # Application definition
